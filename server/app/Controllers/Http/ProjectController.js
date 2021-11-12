2f9a12cff4934764a372153d4a8105badb66a507
dbosnjak6 <dominik.bosnjak@fina.hr>
2021-11-12 08:10:57 +0100

    kreiranje novog projekta

--- server/app/Controllers/Http/ProjectController.js
+++ server/app/Controllers/Http/ProjectController.js
@@ -1,13 +1,35 @@
 'use strict'
 
+const Project = use('App/Models/Project');
+
 class ProjectController {
 
    async index( {auth} ){
+       //dohvatimo korisnika
         const user = await auth.getUser();
+        //ispišemo id
         console.log(user.id);
-        return {
-            message: "hello",
-        }
+        //dohvatimo i vratimo sve njegove projekte
+        return await user.projects().fetch();
+    }
+
+    async create({ auth, request }){
+        //dohvatimo korisnika
+        const user = await auth.getUser();
+        //dohvatimo naslov iz requesta
+        const { title } = request.all();
+        //kreiramo projekt
+        const project = new Project();
+        //ispunimo naslov projekta sa dohvaćenim naslovom iz requesta
+        project.fill({
+            title,
+        });
+
+        //povežemo projekt za korisnika
+        await user.projects().save(project);
+        //vratimo projekt
+        return project;
+
     }
 }
 
--- server/start/routes.js
+++ server/start/routes.js
@@ -21,6 +21,7 @@ Route.group(()=>{
   Route.post('auth/register', 'UserController.register');
   Route.post('auth/login','UserController.login');
   Route.get('projects', 'ProjectController.index').middleware('auth');
+  Route.post('projects', 'ProjectController.create').middleware('auth');
 
 })
 .prefix('api')

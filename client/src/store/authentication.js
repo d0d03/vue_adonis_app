82396377d8b266780f851b12e2ae25c1fae48f90
dbosnjak6 <dominik.bosnjak@fina.hr>
2021-12-07 13:51:05 +0100

    login

--- client/src/store/authentication.js
+++ client/src/store/authentication.js
@@ -6,10 +6,14 @@ export default{
     state:{
         registerEmail: "hello",
         registerPassword: null,
-        token: null,
         registerError: null,
+        loginEmail: "hello",
+        loginPassword: null,
+        loginError: null,
+        token: null,
     },
     actions:{
+        //TODO: apstrahirat register i login u helper funkciju
         register({ commit, state }){
             commit('setRegisterError', null);
             return HTTP().post('auth/register', {
@@ -26,6 +30,19 @@ export default{
             commit('setToken',null);
             router.push('/login')
         },
+        login({ commit, state }){
+            commit('setLoginError', null);
+            return HTTP().post('auth/login', {
+                email: state.loginEmail,
+                password: state.loginPassword,
+            }).then(({ data }) => {
+                commit('setToken', data.token);
+                router.push('/');
+            }).catch(()=>{
+                commit('setLoginError', 'An error has ocured trying to login your accoutn');
+            });
+        },
+
     },
     getters:{
         isLoggedIn(state){
@@ -33,17 +50,26 @@ export default{
         }
     },
     mutations: {
-        setRegisterError(state, error){
-            state.registerError = error;
-        },
         setToken(state, token){
             state.token = token;
+        },
+        setRegisterError(state, error){
+            state.registerError = error;
         },
         setRegisterEmail(state, email){
             state.registerEmail = email;
         },
         setRegisterPassword(state, password){
             state.registerPassword = password;
+        },
+        setLoginError(state, error){
+            state.loginError = error;
+        },
+        setLoginEmail(state, email){
+            state.loginEmail = email;
+        },
+        setLoginPassword(state, password){
+            state.loginPassword = password;
         },
     },
 };
